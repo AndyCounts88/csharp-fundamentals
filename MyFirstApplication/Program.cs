df45b4b65502149f18138768b89cd313de03304e
Andrew Fairchild <AndrewMichaelFairchild88@gmail.com>
2023-07-14 11:20:47 -0500

    Hope I have this right..

--- MyFirstApplication/Program.cs
+++ MyFirstApplication/Program.cs
@@ -11,10 +11,26 @@ HouseExample();
  Multi-line comment
 The below statement is a local function. Local functions are inside of members and are private by default. This means they are specific to that member and can only be called inside that member.
  */
+
 void HouseExample()
 {
     House myHouse = new House();
     myHouse.DoorOpenClose();
 }
 
+MyLocalFunction();
+
+/*
+ This local function is for my first exercise lab.
+ */
+
+Console.WriteLine("This is my first sentence");
+
+Console.WriteLine("I am new to CSharp");
+
+Console.WriteLine("This CSharp course is cool");
+
+Console.WriteLine("I am learning stuff every day.");
+
+void MyLocalFunction() { }
 
